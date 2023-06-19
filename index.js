070e0160a36a717df5e122791d343c0c32a83bd0
aluochvivian <aluochviviann@gmail.com>
2023-06-19 14:07:51 +0300

    Completed assignment

--- index.js
+++ index.js
@@ -1,15 +1,23 @@
-function receivesAFunction(callback) {
-    callback();
-  }
-  
-  function returnsANamedFunction() {
-    return function namedFunction() {
-      // Function body
-    };
-  }
-  
-  function returnsAnAnonymousFunction() {
-    return function() {
-      // Function body
-    };
-  }
+const returnFirstTwoDrivers = function (drivers) {
+  return drivers.slice(0, 2);
+};
+
+const returnLastTwoDrivers = function (drivers) {
+  return drivers.slice(-2);
+};
+
+const selectingDrivers = [returnFirstTwoDrivers, returnLastTwoDrivers];
+
+const createFareMultiplier = function (multiplier) {
+  return function (fare) {
+    return fare * multiplier;
+  };
+};
+
+const fareDoubler = createFareMultiplier(2);
+
+const fareTripler = createFareMultiplier(3);
+
+const selectDifferentDrivers = function (drivers, selectorFunction) {
+  return selectorFunction(drivers);
+};
